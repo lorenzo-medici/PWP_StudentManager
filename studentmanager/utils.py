36e43807f3793029d32e5430d11cc2f4263acf7d
lorenzo-medici <lo.medici@outlook.com>
2023-02-23 11:38:52 +0200

    Added ssn validation and generator in studentmanager/utils.py

---
+++ studentmanager/utils.py
@@ -0,0 +1,33 @@
+import random
+import re
+
+
+def is_valid_ssn(ssn, date_of_birth):
+    return re.fullmatch('([0-9]{6})([A\\-+])(00[2-9]|0[1-9][0-9]|[1-8][0-9][0-9])([0-9A-FHJ-NPR-Y])', ssn) is not None \
+        and ssn[0:6] == date_of_birth.strftime("%d%m%y") \
+        and ssn[10] == generate_control_character(ssn) \
+        and ssn[6] == generate_century_character(date_of_birth)
+
+
+def generate_control_character(ssn):
+    char_list = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'H', 'J', 'K', 'L',
+                 'M', 'N', 'P', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y']
+    num = float(ssn[0:6] + ssn[7:10])
+    mod = round(num % 31)
+    return char_list[mod]
+
+
+def generate_century_character(date):
+    year_digits = date.year // 100
+    year_map = {18: '+', 19: '-', 20: 'A'}
+
+    return year_map[year_digits]
+
+
+def generate_ssn(date):
+    date_string = date.strftime("%d%m%y")
+    century_character = generate_century_character(date)
+    serial_number = random.randrange(2, 900)
+    partial_ssn = f'{date_string}{century_character}{serial_number:03d}'
+    control_character = generate_control_character(partial_ssn)
+    return f'{partial_ssn}{control_character}'
