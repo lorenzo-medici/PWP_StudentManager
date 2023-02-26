68a3a11219909eafaa2de8b29be14b6260698a52
lorenzo-medici <lo.medici@outlook.com>
2023-02-26 18:16:48 +0200

    Created api_test fixture

---
+++ tests/api_test.py
@@ -0,0 +1,127 @@
+# based on http://flask.pocoo.org/docs/1.0/testing/
+# we don't need a client for database testing, just the db handle
+import datetime
+import os
+import tempfile
+
+import pytest
+
+from studentmanager import create_app, db
+from studentmanager.models import Assessment, Student, Course
+
+
+@pytest.fixture
+def client():
+    db_fd, db_fname = tempfile.mkstemp()
+    config = {
+        "SQLALCHEMY_DATABASE_URI": "sqlite:///" + db_fname,
+        "TESTING": True
+    }
+
+    app = create_app(config)
+
+    with app.app_context():
+        db.create_all()
+        _populate_db()
+
+    yield app.test_client()
+
+    with app.app_context():
+        db.session.remove()
+
+    os.close(db_fd)
+    os.unlink(db_fname)
+
+
+def _populate_db():
+    s1 = Student(
+        first_name='Draco',
+        last_name='Malfoy',
+        date_of_birth=datetime.date.fromisoformat('1980-06-05'),
+        ssn='050680-6367'
+    )
+
+    s2 = Student(
+        first_name='Harry',
+        last_name='Potter',
+        date_of_birth=datetime.date.fromisoformat('1980-07-31'),
+        ssn='310780-6176'
+    )
+
+    s3 = Student(
+        first_name='Hermione',
+        last_name='Granger',
+        date_of_birth=datetime.date.fromisoformat('1979-09-19'),
+        ssn='190979-8400'
+    )
+
+    c1 = Course(
+        title='Transfiguration',
+        teacher='Minerva Mcgonagall',
+        code='004723',
+        ects=5
+    )
+
+    c2 = Course(
+        title='Defence Against the Dark Arts',
+        teacher='Professur Severus Snape',
+        code='006031',
+        ects=8
+    )
+
+    a_s1_c1 = Assessment(
+        student=s1,
+        course=c1,
+        grade=5,
+        date=datetime.date.fromisoformat('1993-02-08')
+    )
+
+    a_s1_c2 = Assessment(
+        student=s1,
+        course=c2,
+        grade=4,
+        date=datetime.date.fromisoformat('1993-02-17')
+    )
+
+    a_s2_c1 = Assessment(
+        student=s2,
+        course=c1,
+        grade=3,
+        date=datetime.date.fromisoformat('1993-02-08')
+    )
+
+    a_s2_c2 = Assessment(
+        student=s2,
+        course=c2,
+        grade=4,
+        date=datetime.date.fromisoformat('1993-02-17')
+    )
+
+    a_s3_c1 = Assessment(
+        student=s3,
+        course=c1,
+        grade=5,
+        date=datetime.date.fromisoformat('1993-02-08')
+    )
+
+    a_s3_c2 = Assessment(
+        student=s3,
+        course=c2,
+        grade=5,
+        date=datetime.date.fromisoformat('1993-02-17')
+    )
+
+    db.session.add(s1)
+    db.session.add(s2)
+    db.session.add(s3)
+
+    db.session.add(c1)
+    db.session.add(c2)
+
+    db.session.add(a_s1_c2)
+    db.session.add(a_s2_c1)
+    db.session.add(a_s2_c2)
+    db.session.add(a_s3_c1)
+    db.session.add(a_s3_c2)
+
+    db.session.commit()
