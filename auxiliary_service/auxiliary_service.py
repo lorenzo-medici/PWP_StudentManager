d25d39a8ee8c2736247a64f10dfd55456c7a6915
lorenzo-medici <lo.medici@outlook.com>
2023-04-25 16:14:00 +0300

    Completed implementation of auxiliary service

--- auxiliary_service/auxiliary_service.py
+++ auxiliary_service/auxiliary_service.py
@@ -1,35 +1,53 @@
+"""
+This file contains the auxiliary service for our project.
+It declares one route 'studentCard/<student_id>', initializes and starts the server.
+"""
 import base64
 import os
 import sys
+from io import BytesIO
 
 import requests
+from PIL import Image, ImageFont, ImageDraw
 from flask import Flask, Response
 
 app = Flask(__name__)
-server_url = ''
-student_collection_href = ''
+SERVER_URL = ''
+STUDENT_COLLECTION_HREF = ''
+
+GET_TIMEOUT = 30
 
 
-@app.route('/studentCard/<student_id>/')
+@app.route('/studentCard/<int:student_id>/')
 def student_card_generator(student_id):
-    student_collection = requests.get(server_url + student_collection_href)
+    """
+    This function responds to GET requests on the '/studentCard/<student_id>' endpoint.
+    It requests data from the StudentManager API about the student whose student_id is passed as
+        parameter, and generates and returns the image of their student card, complete with their
+        profile picture, full name, date of birth and student id.
+    The student_id provided has to exist in the StudentManager API, otherwise error code 404
+        will be returned.
+    :param student_id: An integer representing an existing student_id in the API
+    :return: 404 if the student_id doesn't exist
+    :return: 503 if the StudentManager API answers with stuatus code different than 200
+    :return: 200 and the image of the student card, if no error happens
+    """
+    student_collection = requests.get(SERVER_URL + STUDENT_COLLECTION_HREF, timeout=GET_TIMEOUT)
 
     if student_collection.status_code != 200:
         return Response(f"API server responded with code {student_collection.status_code}", 503)
 
     student_href = ''
 
-    collection_body = student_collection.json()
-
     for student in student_collection.json()["items"]:
-        if str(student["student_id"]) == student_id:
+        if str(student["student_id"]) == str(student_id):
             student_href = student["@controls"]["self"]["href"]
             break
 
     if student_href == '':
         return Response(f"Given student id {student_id} not found on the API server", 404)
 
-    student_page = requests.get(server_url + student_href)
+    student_page = requests.get(SERVER_URL + student_href, timeout=GET_TIMEOUT)
 
     if student_page.status_code != 200:
         return Response(f"API server responded with code {student_collection.status_code}", 503)
@@ -40,34 +58,54 @@ def student_card_generator(student_id):
     surname = student_body["last_name"]
     date_of_birth = student_body["date_of_birth"]
 
-    picture_href = student_body["@controls"]["studman:propic"]["href"]
-
-    picture_page = requests.get(server_url + picture_href)
+    picture_page = requests.get(SERVER_URL + student_body["@controls"]["studman:propic"]["href"],
+                                timeout=GET_TIMEOUT)
 
     if picture_page.status_code != 200:
         return Response(f"API server responded with code {student_collection.status_code}", 503)
 
-    picture_body = picture_page.json()
-
-    # image_data contains exactly the same data you would read from a file
+    # propic_data contains exactly the same data you would read from a file
     # You can use it to compose the student card (probably encapsulate in an Image class
     #   or something similar)
-    image_data = base64.b64decode(picture_body["picture"])
+    propic_bytes = BytesIO(base64.b64decode(picture_page.json()["picture"]))
 
     # Compose student card
+    final_image = Image.open('static/Background.png')
 
-    student_card = ''
+    final_image.paste(Image.open(propic_bytes), (50, 50))
 
-    # for returning, export the student card image as jpeg into the student_card variable
+    drawer = ImageDraw.Draw(final_image)
+    my_font = ImageFont.truetype("static/Helvetica.ttf", size=40)
+
+    drawer.text((600, 150), 'Full name:', font=my_font, fill=(0, 0, 0))
+    drawer.text((630, 200), f'{name} {surname}', font=my_font, fill=(0, 0, 0))
+
+    drawer.text((600, 275), 'Date of birth:', font=my_font, fill=(0, 0, 0))
+    drawer.text((630, 325), f'{date_of_birth}', font=my_font, fill=(0, 0, 0))
+
+    drawer.text((600, 400), 'Student ID:', font=my_font, fill=(0, 0, 0))
+    drawer.text((630, 450), f'{student_id}', font=my_font, fill=(0, 0, 0))
+
+    # for returning, export the student card image as jpeg into the payload variable
     #   (it must contain the data you would directly write to a file), then simply return
-    return Response(image_data, 200, mimetype='image/jpeg')
+    payload = BytesIO()
+    final_image.save(payload, 'JPEG', quality=100)
+    payload.seek(0)
+
+    return Response(payload, 200, mimetype='image/jpeg')
 
     # then on the client side, you simply need to do
-    # resp = requests.get(f'{auxiliary_server_url}/studentCard/{student_id}/')
-    # with open(f'StudentCard_{student_id}.jpeg', 'wb') as f:
-    #     f.write(resp.content)
+    # resp = requests.get(f'{auxiliary_server_url}/studentCard/{student_id}/', timeout=GET_TIMEOUT)
+    # student_card_image = Image.open(BytesIO(base64.b64decode(resp.content)))
     #
-    # Now the file contains the student card image
+    # Then you can do student_card_image.show() to open a window with the picture in it
+    #   or student_card_image.save(<path>, 'JPEG', quality=100) to save it to a file
+
+    # You can do the same thing for the profile picture image that you get from the API
+    # The only thing that you need to change is
+    # picture_body = picture_page.json()
+    # and use picture_body["picture"] instead of resp.content
+    # If you need help ask Lorenzo
 
 
 if __name__ == '__main__':
@@ -75,29 +113,29 @@ if __name__ == '__main__':
     if len(sys.argv) != 3:
         print(f"Invalid number of arguments. \
         Usage: python {os.path.basename(__file__)} <port> <apiUrl>")
-        exit(-1)
+        sys.exit(-1)
 
     try:
-        port = int(sys.argv[1])
-        if port < 1024 or port > 49151:
-            print("Local port not in valid range [1024, 49151]!")
-            exit(-1)
+        PORT = int(sys.argv[1])
+        if PORT < 1024 or PORT > 49151:
+            print("Local PORT not in valid range [1024, 49151]!")
+            sys.exit(-1)
 
-        server_url = sys.argv[2]
-        api_entrypoint = requests.get(server_url + '/api/')
+        SERVER_URL = sys.argv[2]
+        api_entrypoint = requests.get(SERVER_URL + '/api/', timeout=GET_TIMEOUT)
 
         if api_entrypoint.status_code != 200:
             print(f"Error {api_entrypoint.status_code} on remote server")
-            exit(-3)
+            sys.exit(-3)
 
-        student_collection_href = api_entrypoint.json()["@controls"]["studman:students-all"]["href"]
+        STUDENT_COLLECTION_HREF = api_entrypoint.json()["@controls"]["studman:students-all"]["href"]
 
-        app.run(host="localhost", port=port)
+        app.run(host="localhost", port=PORT)
 
     except ValueError as e:
-        print(f"Invaid port paramter: not an integer!{e.with_traceback()}")
-        exit(-1)
+        print(f"Invaid PORT paramter: not an integer!{e.with_traceback()}")
+        sys.exit(-1)
 
     except requests.ConnectionError:
-        print(f'URL does not point to the StudentManager API: {server_url}')
-        exit(-2)
+        print(f'URL does not point to the StudentManager API: {SERVER_URL}')
+        sys.exit(-2)
